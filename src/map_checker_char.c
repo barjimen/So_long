be1a200d242bc71d7e90f9fb3d0dd4031d86f098
barjimen <barby.jimenez17@gmail.com>
2024-06-05 23:03:18 +0200

    Parse 85%

--- src/map_checker_char.c
+++ src/map_checker_char.c
@@ -1,28 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   map_checker_char.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 20:26:01 by barjimen          #+#    #+#             */
-/*   Updated: 2024/06/04 23:34:48 by barjimen         ###   ########.fr       */
+/*   Created: 2024/06/04 23:31:39 by barjimen          #+#    #+#             */
+/*   Updated: 2024/06/04 23:58:31 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-int main(int argc, char **argv)
+void is_char(char **map,int width, int height)
 {
-    char **mapita;
-   // int i = 0;
-    
-    mapita = NULL;
-    mapita = arg_handler(argc, argv, mapita);
-    //printf("mapita es en main: %s\n", mapita[1]);
-    if (map_check(mapita))
-      exit(1);
-    
-    //render_map(mapita);
-    exit(0);
+	static int c;
+	static int e;
+	static int p;
+    static int h;
+    static int w;
+	
+	while (h < height)
+	{
+		while (w < width)
+		{	
+			if (map[h][w] == 'C')
+				c++;
+			else if (map[h][w] == 'P')
+				p++;
+			else if (map[h][w] == 'E')
+				e++;
+			w++;
+		}
+		w = 0;
+		h++;
+	}
+	if (c < 1 || p != 1 || e != 1)
+		exit (1);
 }
