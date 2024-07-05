eca906c0b11cb4317fda9cf425323715d07b857f
barjimen <barby.jimenez17@gmail.com>
2024-07-05 22:54:58 +0200

    window moves + collection

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 23:26:47 by barjimen          #+#    #+#             */
-/*   Updated: 2024/07/05 22:44:48 by barjimen         ###   ########.fr       */
+/*   Updated: 2024/07/05 22:54:34 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,27 @@
 void render_move(t_so_long *so_long)
 {
 	char *moves;
-	
+	char *collecti;
+	char *a;
+	char *b;
+
+	a = ft_itoa(so_long->player.player_moves);
+	b = ft_itoa(so_long->player.collect_num);
 	so_long->player.player_moves += 1;
-	moves = ft_itoa(so_long->player.player_moves);
-	printf("Se ha movido %s veces\n", moves);
+	moves = ft_strjoin("Numero de movimientos: " ,a);
+	collecti = ft_strjoin("Numero de coleccionables: " ,b);
+	//printf("Se ha movido %s veces\n", moves);
 	item_removed(so_long);
 	map_iter_context(so_long->map, create_background, so_long);
 	map_iter_context(so_long->map, create_items, so_long);
 	create_player(so_long);
 	mlx_put_image_to_window(so_long->mlx_data.mlx_ptr,so_long->mlx_data.win_ptr, so_long->mlx_data.img.ptr, 0, 0);
 	mlx_string_put(so_long->mlx_data.mlx_ptr, so_long->mlx_data.win_ptr, 64, 64, 0xFFFFFF, moves);
+	mlx_string_put(so_long->mlx_data.mlx_ptr, so_long->mlx_data.win_ptr, 64, 128, 0xFFFFFF, collecti);
 	free(moves);
+	free(collecti);
+	free(a);
+	free(b);
 }
 
 void move_player(t_so_long *so_long, int new_x, int new_y)
